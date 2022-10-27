a32981fc53988b7acdf41b3b6e4f70e67f2e2a2e
Redas Krikščiukaitis <redas.kriksciukaitis@gmail.com>
2022-10-27 08:55:30 +0300

    Add initial Items.cs

---
+++ src/TimiTrack.Domain/Items/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimiTrack.Items;
+using Volo.Abp.Domain.Entities.Auditing;
+
+public class Items
+{
+    public class Item : AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public ItemType Type { get; set; }
+
+        public DateTime ScrapeDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
